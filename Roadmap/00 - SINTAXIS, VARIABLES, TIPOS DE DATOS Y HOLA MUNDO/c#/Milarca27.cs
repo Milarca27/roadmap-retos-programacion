d7e33e957638843a91457573c4b0810179f8ddbd
Milarca27 <mmilarca27@gmail.com>
2025-06-03 23:16:58 -0600

    Create Milarca27.cs

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/Milarca27.cs
@@ -0,0 +1,33 @@
+//Paso 1:
+
+//https://learn.microsoft.com/es-es/dotnet/csharp/
+
+//Paso 2:
+
+// Este es una forma de hacer comentarrios
+
+/* */
+Esta es otra forma de hacer 
+comentarios, 
+pero,
+utilizando varias 
+lineas
+/* */
+
+//Paso 3:
+
+int variablePractica1 = 3;
+
+// const + tipodedato + NOMBRE(en MAYUSCULAS) = valor;
+const int CONSTANTE_PRACTICA = 40;
+
+//Paso 4:
+
+int variablePractica2 = 4;
+float varieblePractica3 = 6.3;
+string mensajePractica = "Mensaje de Practica";
+bool booleanoPractica = true;
+
+//Paso 5:
+
+Console.WriteLine("¡Hola, C#");
